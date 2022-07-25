b4080acfcde73ad3f333b0c6635713a978018a1a
Premjeet Kumar <pkrsingh2@gmail.com>
2022-07-26 00:58:22 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -2,18 +2,9 @@
 import os
 import numpy as np
 import pickle
-import tensorflow as tf
 
-from tensorflow.keras import layers
-from tensorflow.keras import models,utils
 import pandas as pd
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import load_img,img_to_array
-from tensorflow.python.keras import utils
 
-from tensorflow.keras.applications.vgg16 import VGG16
-from tensorflow.keras.layers import Dense, Dropout, Flatten, Input, GlobalMaxPool2D
-from tensorflow.keras.models import Model
 
 current_path = os.getcwd()
 
